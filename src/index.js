b5721750384eaf1faa708cb6cd0509269efada9d
Nguyen The Anh <nguyentheanh17071998@gmail.com>
2021-04-21 20:42:27 +0700

    update info

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {I18nextProvider} from 'react-i18next';
-import i18next from 'i18next';
-import common_vi from "./translations/vi/common.json";
-import common_en from "./translations/en/common.json";
-import common_ja from "./translations/ja/common.json"
-import * as serviceWorker from './serviceWorker';
-
-i18next.init({
-  interpolation: { escapeValue: false },  // React already does escaping
-  lng: 'vi',                              // language to use
-  resources: {
-      en: {
-          common: common_en               // 'common' is our custom namespace
-      },
-      vi: {
-          common: common_vi
-      },
-      ja: {
-          common: common_ja
-      }
-  },
-});
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./i18next";
 
 ReactDOM.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
+    <Suspense fallback="loading">
       <App />
-    </I18nextProvider>
+    </Suspense>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
